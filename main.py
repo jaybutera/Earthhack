47d708bb59e7465c8e9b9edda1eb6fe359b4a779
Jay Butera <buterajay@gmail.com>
2019-04-28 02:02:29 -0500

    Sim runs without errors

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import matplotlib.pyplot
 import numpy as np
 from PIL import Image
+from sim import *
 
+'''
+def evaluate():
+
+def _fitness(x):
+    sim = Sim(bmp)
+    for i in range(0,20):
+        sim.update()
+'''
+
+'''
 im = Image.open('./heightmap.png')
 b = np.array(im)
+bmp = b[:,:,0]
 print(b.shape)
-print(b[:,:,0])
+'''
+
+X = np.arange(8*8).reshape((8,8))
+source = [7,7]
+
+s = Sim(X, source)
+for i in range(0,5):
+    s.step()
