715dfa7d8a82a62d3df680641cab9c396e040812
Jay Butera <buterajay@gmail.com>
2019-04-27 19:31:53 -0500

    get_maxima fn

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot
 import numpy as np
 from PIL import Image
+from scipy.ndimage import maximum_filter
 
-im = Image.open('./heightmap.png')
-b = np.array(im)
-print(b.shape)
-print(b[:,:,0])
+def get_maxima(bmp):
+    mx   = maximum_filter(bmp, size=3)
+    diff = np.where(mx == bmp, bmp, 0)
+    res  = np.where(diff != 0)
+
+    return np.array([ (res[0][i], res[1][i]) for i,_ in enumerate(res[0]) ])
+
+bitmap = np.array( Image.open('./heightmap.png') )[:,:,0]
+(N,M) = bitmap.shape
+print(bitmap.shape)
+
+print( get_maxima(bitmap) )
